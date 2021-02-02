b856edb8daca14445b62fdae577ccde15719fd33
Simran53999 <37632558+Simran53999@users.noreply.github.com>
2021-02-02 12:01:59 +0530

    update

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello world, we still doin it");
 //		System.out.println("Hi");(ctrl+/ for single line comment, for multiple line to comment select the lines and )
 
 	}
